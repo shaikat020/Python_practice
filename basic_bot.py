51bb3e9e462d36ab333264585d078afe331ba67f
shaikat020 <tahsin.azad.skt@gmail.com>
2025-05-02 19:28:44 +0600

    Basic chatbot

---
+++ basic_bot.py
@@ -0,0 +1,13 @@
+while True:
+    msg=input("You : ")
+    if "hello" in msg.lower():
+        print("Bot : Hi there. How are you?")
+    elif "fine" in msg.lower():
+        print("Bot : Good")
+    elif "what about you" in msg.lower():
+        print("Bot : I am fine, too. What can I help?")
+    elif "bye" in msg.lower():
+        print("Bot : Ok, bye. Have a good day")
+        break
+    else:
+        print("Bot : I don't understand what are you saying")
