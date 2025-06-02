0a240e58865c45e681864ff3cae66064d7bd103d
shaikat020 <tahsin.azad.skt@gmail.com>
2025-06-03 01:29:50 +0600

    Currency converter

---
+++ currency2converter.py
@@ -0,0 +1,16 @@
+from currency_converter import CurrencyConverter
+
+def convert_currency(amount, from_currency, to_currency):
+    c = CurrencyConverter()
+    result = c.convert(amount, from_currency, to_currency)
+    return result
+
+amount= float(input("Enter the amount to convert : "))
+
+from_currency= input("Enter the currency to convert from : ").upper()
+
+to_currency= input("Enter the currency to convert from : ").upper()
+
+convert_amount = convert_currency(amount, from_currency, to_currency)
+
+print(f"{amount} {from_currency} = {convert_amount} {to_currency}")
