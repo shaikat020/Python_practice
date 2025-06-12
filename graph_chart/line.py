3bd7f19db1aa0725dd8fee826a575233849718f6
shaikat020 <tahsin.azad.skt@gmail.com>
2025-06-12 13:09:48 +0600

    Add user input for chart labels and data points in plotting script

--- graph_chart/line.py
+++ graph_chart/line.py
@@ -1,11 +1,25 @@
 import matplotlib.pyplot as plt
 
-x=  [1, 2, 3, 4, 5]
+x=  []
 
-y= [45, 21, 33, 22, 21]
+y= []
+
+chart_title = input("Enter the chart title: ")
+x_label = input("Enter the x-axis label: ")
+y_label = input("Enter the y-axis label: ")
+
+n = int(input("Enter the number of data points: "))
+
+for i in range(n):
+    xi = float(input(f"Enter x value for point {i+1}: "))
+    yi = float(input(f"Enter y value for point {i+1}: "))
+    
+    #append data into list
+    x.append(xi)
+    y.append(yi)
 
 plt.plot(x,y)
 plt.title('Line chart')
-plt.xlabel('x-axis')
-plt.ylabel('y-axis')
+plt.xlabel(x_label)
+plt.ylabel(y_label)
 plt.show()
