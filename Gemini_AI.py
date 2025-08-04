d8c9eb8f0cda036111950c2b08035714b70222b2
shaikat020 <tahsin.azad.skt@gmail.com>
2025-08-04 10:31:56 +0600

    Requirements.txt and user input response AI

--- Gemini_AI.py
+++ Gemini_AI.py
@@ -1,5 +1,7 @@
+# Necessary module
 # pip install -q -U google-genai
 # pip install python-dotenv
+
 from dotenv import load_dotenv
 import os
 
@@ -7,9 +9,22 @@ from google import genai
 load_dotenv()
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
+
+# API key must be provided in .env file
 client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
+print("Start chatting with the AI! Type 'goodbye' to stop.")
+
+
+while True:
+    user_input = input("You: ")
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=user_input
+    )
+    print("AI: ",response.text)
+    if user_input.lower() == 'goodbye':
+        break
+
+print("Chat ended.")
+
