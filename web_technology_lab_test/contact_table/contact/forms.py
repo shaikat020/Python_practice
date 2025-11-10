5628c256e0ab9cdee29a4c0ad08004bef2ef7ab4
shaikat020 <tahsin.azad.skt@gmail.com>
2025-11-10 22:29:00 +0600

    Refractor: Delete garbage

--- web_technology_lab_test/contact_table/contact/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import contactinfo
-
-class contactinfoform(forms.ModelForm):
-    class Meta:
-        model=contactinfo
-        fields=['name', 'email', 'mobile', 'address']
